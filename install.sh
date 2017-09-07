a7fc45a97538e2509627993b337371833dc61f8e
Thomas Baeckeroot <tbaecker.spam@gmail.com>
2017-09-07 22:41:59 +0200

    bug resolving

--- install.sh
+++ install.sh
@@ -44,13 +44,16 @@ if [ "$defaultadmin" == "" ]; then
 	term=`tty`
 	defaultadmin=`ls -l $term | awk '{print $3}'`
 fi
-
+if [ -f /root/parental_control_admin.cfg ]; then
+	PREVIOUS_ADMIN=`sudo cat /root/parental_control_admin.cfg`
+	echo "Information: '$PREVIOUS_ADMIN' as been previously informed as administrator."
+fi
 read -p "Define user who would be administrator [default=$defaultadmin] :" ADMIN
 if [ "$ADMIN" == "" ]; then
 	ADMIN=$defaultadmin
 fi
-
-sudo echo $ADMIN >/root/parental_control_admin.cfg
+sudo echo $ADMIN >/tmp/parental_control_admin.cfg
+sudo mv /tmp/parental_control_admin.cfg /root/parental_control_admin.cfg
 
 # creates configuration files in admin user's home folder:
 USERS_AND_TIMES_FILE=/home/$ADMIN/users_and_times.cfg
@@ -60,14 +63,14 @@ echo "# Configuration file containing usernames" >>$USERS_AND_TIMES_FILE
 echo "# and their corresponding alloted time." >>$USERS_AND_TIMES_FILE
 echo "# Format: <login-name> <allocated minutes>" >>$USERS_AND_TIMES_FILE
 echo >>$USERS_AND_TIMES_FILE
-
+echo
 echo "What should be the time limit for each user, in minutes per day?"
 echo "- a default value of 60 minutes per day is used,"
 echo "  except for the administrator who has unlimited time -"
 for VICTIM in $VICTIMS; do
-	DEFAULT_TIME_LIMIT = 60
+	DEFAULT_TIME_LIMIT=60
 	if [ "$VICTIM" == "$ADMIN" ]; then
-		DEFAULT_TIME_LIMIT = 9999
+		DEFAULT_TIME_LIMIT=9999
 		echo "Note that $DEFAULT_TIME_LIMIT is informed for administrator in"
 		echo "configuration file but this value is actually ignored..."
 	fi
@@ -79,7 +82,7 @@ for VICTIM in $VICTIMS; do
 	# We're done for this victim!
 done
 # We're done for all victims!
-
+sudo chown $ADMIN:$ADMIN $USERS_AND_TIMES_FILE
 echo
 echo "Configuration file created in $USERS_AND_TIMES_FILE ."
 echo "Terminated."
